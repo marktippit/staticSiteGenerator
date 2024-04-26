ad3c6fad839f304e7f7bbc685b8efbc408462ca1
marktippit <marktippit@gmail.com>
2024-04-26 09:56:33 -0400

    inline markdown complete

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -10,6 +10,14 @@ from textnode import (
     text_type_link
 )
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    return nodes
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
